d6de848d0a4439050aad5fd4c562f6fbdbad4c9b
Rawan.H <rawanhawangledt@gmail.com>
2024-03-01 21:14:36 -0300

    Update script.js

--- JavaScript/Events/Exercicio05/script.js
+++ JavaScript/Events/Exercicio05/script.js
@@ -1,11 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button1 = document.getElementById('button1');
+    const button2 = document.getElementById('button2');
+    const button3 = document.getElementById('button3');
 
     button1.addEventListener('click', function() {
-        changeBackgroundColor('red');
+        changeBackgroundColor('vermelho');
+    });
+
+    button2.addEventListener('click', function() {
+        changeBackgroundColor('azul');
+    });
+
+    button3.addEventListener('click', function() {
+        changeBackgroundColor('verde');
     });
 
     function changeBackgroundColor(color) {
-        //Altera a cor do background da página
+        switch (color) {
+            case 'vermelho':
+                document.body.style.backgroundColor = 'red'
+                break;
+
+            case 'azul':
+                document.body.style.backgroundColor = 'blue'
+                break;
+
+            case 'verde':
+                document.body.style.backgroundColor = 'green'
+                break;
+        
+            default:
+                break;
+        }
     }
 });
